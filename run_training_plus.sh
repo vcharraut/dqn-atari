e1891272d2a00078cbe16019fb0d4dd36888ef6a
Valentin <valentin.canete@gmail.com>
2020-01-01 18:03:31 +0100

    fix script

--- run_training_plus.sh
+++ run_training_plus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 1 --mse 1 &
-nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 0 --mse 1 &
-nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 1 --mse 0 &
-nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 0 --mse 0 &
+nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 1 --mse 1
+nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 0 --mse 1
+nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 1 --mse 0
+nohup sudo python3 learn.py --doubleq 1 --dueling 1 --adam 0 --mse 0
