42508c9bd68f1f613583d6b3924cc3b1ffe5b28d
Valentin <valentin.canete@gmail.com>
2020-01-05 18:09:21 +0100

    fix for merge

--- play.py
+++ play.py
@@ -1,33 +1,41 @@
-import gym
-import torch
+import argparse
 
-from playground.atari.config import Config
+from playground.atari.config import Config, ConfigRainbow
+from playground.atari.algorithm.rainbow import Rainbow
 from playground.atari.algorithm.dqn import DQN
+from playground.cartpole.dqn_cartpole import DQN_Cartpole
 
 
-config = Config()
-agent = DQN('BreakoutNoFrameskip-v4', config)
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, default='', help='')
+parser.add_argument('--algo', type=str, default='', help='')
+parser.add_argument('--display', type=int, default=1, help='')
+parser.add_argument('--record', type=int, default=0, help='')
 
-agent.model.load_state_dict(torch.load('playground/atari/save/dqn_huberloss_no.pt'))
-agent.model.cuda()
-agent.model.eval()
 
-for t in range(20):
-	# Run one episode until termination
-	r = 0
-	done = False
-	state = agent.env.reset()
-	while not done:
-		agent.env.render()
 
-		state = torch.from_numpy(state).float()
-		state = state.unsqueeze(0).to(torch.device('cuda'))
-		action = agent.model(state).argmax().item()
+args = parser.parse_args()
 
-		# Get the output of env from this action
-		state, reward, done, _ = agent.env.step(action)
-		r += reward
+display = args.display
+record = args.record
 
-	print(t, r)
-
-agent.env.close()
+if args.env == 'cartpole':
+	agent = DQN_Cartpole(None, 8, None, None, None, evaluation=True, record=record)
+	agent.play(display=display, model_path='playground/cartpole/save/lr=0.01_hidden=8_gamma=0.99_batchsize=256_steptarget=100.pt')
+elif args.env == 'atari':
+	if args.algo == 'dqn':
+		print("No model saved yet.")
+		config = Config()
+		agent = DQN('BreakoutNoFrameskip-v4', config, False, False, False, True, evaluation=True, record=record)
+	elif args.algo == 'dqn+':
+		print("No model saved yet.")
+		config = Config()
+		agent = DQN('BreakoutNoFrameskip-v4', config, True, True, False, True, evaluation=True, record=record)
+	elif args.algo == 'rainbow':
+		config = ConfigRainbow()
+		agent = Rainbow('BreakoutNoFrameskip-v4', config, True, evaluation=True, record=record)
+		agent.test(display=display, model_path='playground/atari/save/rainbow_adam_1-final.pt')
+	else:
+		raise ValueError('Algo is not valid')
+else:
+	raise ValueError('Environnement is not valid')
